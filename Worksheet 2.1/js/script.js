cd85f2f2eca5810a8b500f9af06b1f461c481a89
Matchoo95 <mattmatth123@googlemail.com>
2016-09-16 17:26:02 +0100

    Add files via upload

---
+++ Worksheet 2.1/js/script.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+document.bgColor="Red";
+{
+var firstname = "Matthew";
+var lastname = " Hawkins";
+var fullname = firstname + lastname;
+document.write("First Name:");
+document.write(<br>"Last Name:"</br>);
+document.bgColor="Blue";
+}
